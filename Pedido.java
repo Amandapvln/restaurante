de8611375038d99092d264cd665d0015acf874a3
Amanda Paiva Lino <106124434+Amandapvln@users.noreply.github.com>
2023-03-02 10:56:26 -0300

    create a class Pedido

---
+++ Pedido.java
@@ -0,0 +1,17 @@
+public class Pedido {
+    private String nome;
+    private double preco;
+
+    public Pedido(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+}
